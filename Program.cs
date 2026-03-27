b5eb68bdc13a0b9c97a67bf8148d6ee4ade5ffc6
lab_services_student <lab_services_student@LabVM2047235>
2026-03-27 10:32:45 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CyberSecurityChatBot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
